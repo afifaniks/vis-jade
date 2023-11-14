c239bb574100b8b945fef7047a8422f888b703f7
Afif Al Mamun <afifanik@gmail.com>
2023-11-13 19:00:07 -0700

    Initialize Spring Boot project

---
+++ src/main/java/vis/VisJadeApplication.java
@@ -0,0 +1,13 @@
+package vis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisJadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisJadeApplication.class, args);
+    }
+
+}
