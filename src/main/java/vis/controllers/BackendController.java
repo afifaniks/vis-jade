5706d4614ed409b81ec72919779a7a7b4d323f2f
Afif Al Mamun <afifanik@gmail.com>
2023-11-13 23:24:51 -0700

    Add Swagger UI for API documentation

--- src/main/java/vis/controllers/BackendController.java
+++ src/main/java/vis/controllers/BackendController.java
@@ -1,8 +1,8 @@
 package vis.controllers;
 
 import jade.wrapper.ControllerException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import vis.dto.GatewayResponseDto;
 import vis.services.AgentGatewayService;
@@ -15,9 +15,8 @@ public class BackendController {
     }
 
     @GetMapping("/api")
-    public String test() throws ControllerException, InterruptedException {
-        System.out.println("askhdkajsd");
-        GatewayResponseDto responseDto = gatewayService.request("test");
+    public String test(@RequestParam String req) throws ControllerException, InterruptedException {
+        GatewayResponseDto responseDto = gatewayService.request(req);
         return responseDto.getMessage();
     }
 }
