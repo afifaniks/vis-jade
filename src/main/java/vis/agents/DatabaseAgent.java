05536be38eeb4ae144b81a9d59a0860e74c46802
Afif Al Mamun <afifanik@gmail.com>
2023-12-03 01:04:48 -0700

    Implement database agent for write operation

--- src/main/java/vis/agents/DatabaseAgent.java
+++ src/main/java/vis/agents/DatabaseAgent.java
@@ -1,7 +1,22 @@
 package vis.agents;
 
 import jade.core.Agent;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import vis.agents.behaviour.DatabaseBehaviour;
 
 public class DatabaseAgent extends Agent {
+
+	private final Logger logger = LoggerFactory.getLogger(AuthenticationAgent.class);
+
+	@Override
+	protected void setup() {
+		logger.debug("Database agent started. AID: " + getAID().getName());
+		addBehaviour(new DatabaseBehaviour(this));
+	}
 
 }
