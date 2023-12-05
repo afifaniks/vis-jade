ceb446e1610fd09d67069aa3af3749d55b289f94
sayannath <sayannath235@gmail.com>
2023-12-04 18:57:13 -0700

    Added properties in vehicle

--- src/main/java/vis/ontology/predicates/Vehicle.java
+++ src/main/java/vis/ontology/predicates/Vehicle.java
@@ -7,6 +7,13 @@ public class Vehicle implements Concept {
 	String vehicleId;
 
 	String userId;
+	String vehicleName;
+	String vehicleModel;
+	String vehicleType;
+	String licenseNumber;
+	String purchaseDate;
+	String vehicleStatus;
+	float mileage;
 
 	public Vehicle(String vehicleId, String userId) {
 		this.vehicleId = vehicleId;
@@ -31,5 +38,74 @@ public class Vehicle implements Concept {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
+	public String getVehicleName() {
+		return vehicleName;
+	}
+
+	public void setVehicleName(String vehicleName) {
+		this.vehicleName = vehicleName;
+	}
+
+	public String getVehicleModel() {
+		return vehicleModel;
+	}
+
+	public void setVehicleModel(String vehicleModel) {
+		this.vehicleModel = vehicleModel;
+	}
+
+	public String getVehicleType() {
+		return vehicleType;
+	}
+
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+
+	public String getLicenseNumber() {
+		return licenseNumber;
+	}
+
+	public void setLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+
+	public String getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(String purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	public String getVehicleStatus() {
+		return vehicleStatus;
+	}
+
+	public void setVehicleStatus(String vehicleStatus) {
+		this.vehicleStatus = vehicleStatus;
+	}
+
+	public float getMileage() {
+		return mileage;
+	}
+
+	public void setMileage(float mileage) {
+		this.mileage = mileage;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
