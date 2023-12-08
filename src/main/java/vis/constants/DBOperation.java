7abb01e1b1c4be590a4bdac4ff78f4e1d2bb1d35
sayannath <sayannath235@gmail.com>
2023-12-08 01:15:00 -0700

    Reformatted code

--- src/main/java/vis/constants/DBOperation.java
+++ src/main/java/vis/constants/DBOperation.java
@@ -4,39 +4,39 @@ import java.io.Serializable;
 
 public class DBOperation implements Serializable {
 
-	public enum Operation {
+    public enum Operation {
 
-		LOGIN, SIGNUP, GET_PACKAGES, SUBSCRIBE, CLAIM, REGISTER_VEHICLE, GET_USER
+        LOGIN, SIGNUP, GET_PACKAGES, SUBSCRIBE, CLAIM, REGISTER_VEHICLE, GET_USER
 
-	}
+    }
 
-	Operation operation;
+    Operation operation;
 
-	Object additionalObject;
+    Object additionalObject;
 
-	public DBOperation(Operation operation, Object additionalObject) {
-		this.operation = operation;
-		this.additionalObject = additionalObject;
-	}
+    public DBOperation(Operation operation, Object additionalObject) {
+        this.operation = operation;
+        this.additionalObject = additionalObject;
+    }
 
-	public DBOperation(Operation operation) {
-		this.operation = operation;
-	}
+    public DBOperation(Operation operation) {
+        this.operation = operation;
+    }
 
-	public Operation getOperation() {
-		return operation;
-	}
+    public Operation getOperation() {
+        return operation;
+    }
 
-	public void setOperation(Operation operation) {
-		this.operation = operation;
-	}
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
 
-	public Object getAdditionalObject() {
-		return additionalObject;
-	}
+    public Object getAdditionalObject() {
+        return additionalObject;
+    }
 
-	public void setAdditionalObject(Object additionalObject) {
-		this.additionalObject = additionalObject;
-	}
+    public void setAdditionalObject(Object additionalObject) {
+        this.additionalObject = additionalObject;
+    }
 
 }
