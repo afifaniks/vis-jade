f9b563d1e7d63234941be520cda32b06df11e984
sayannath <sayannath235@gmail.com>
2023-12-08 20:20:39 -0700

    Added dob in insurance recommendation

--- src/main/java/vis/util/RecommenderUtil.java
+++ src/main/java/vis/util/RecommenderUtil.java
@@ -5,6 +5,9 @@ import vis.entity.InsurancePackageEntity;
 import vis.entity.VehicleEntity;
 import vis.services.schema.InsurancePackageSchema;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,59 +17,71 @@ import java.util.List;
  */
 public class RecommenderUtil {
 
-	/***
-	 * This method is used to generate recommendations based on the vehicle's mileage.'
-	 * @param vehicleEntity The vehicle entity.
-	 * @param entities The list of entities.
-	 * @return A list of recommendations.
-	 */
-	public static ArrayList<InsurancePackageSchema> generateRecommendation(VehicleEntity vehicleEntity,
-			List<InsurancePackageEntity> entities) {
-		ArrayList<InsurancePackageSchema> recommendations = new ArrayList<>();
+    /***
+     * This method is used to generate recommendations based on the vehicle's mileage.'
+     * @param vehicleEntity The vehicle entity.
+     * @param birthdate The DOB of the user.
+     * @param entities The list of entities.
+     * @return A list of recommendations.
+     */
+    public static ArrayList<InsurancePackageSchema> generateRecommendation(String birthdate, VehicleEntity vehicleEntity, List<InsurancePackageEntity> entities) {
+        ArrayList<InsurancePackageSchema> recommendations = new ArrayList<>();
 
-		if (vehicleEntity == null) {
-			return recommendations;
-		}
+        if (vehicleEntity == null) {
+            return recommendations;
+        }
 
-		List<String> packageForNewCars = Arrays.asList("Premium", "Classic");
-		List<String> packageForOldCars = Arrays.asList("Starter", "Classic");
-		List<String> packageForAverage = Arrays.asList("Basic", "Classic");
+        int userCurrentAge = calculateAge(birthdate);
 
-		for (InsurancePackageEntity insurancePackage : entities) {
-			if (vehicleEntity.getMileage() <= 10000) {
-				if (packageForNewCars.contains(insurancePackage.getPackageName())) {
-					addPackageToList(insurancePackage, recommendations);
-				}
-			}
-			if (vehicleEntity.getMileage() > 10000 && vehicleEntity.getMileage() <= 100000) {
-				if (packageForAverage.contains(insurancePackage.getPackageName())) {
-					addPackageToList(insurancePackage, recommendations);
-				}
-			}
-			if (vehicleEntity.getMileage() > 100000) {
-				if (packageForOldCars.contains(insurancePackage.getPackageName())) {
-					addPackageToList(insurancePackage, recommendations);
-				}
-			}
-		}
+        List<String> packageForNewCars = Arrays.asList("Premium", "Classic");
+        List<String> packageForOldCars = Arrays.asList("Starter", "Classic");
+        List<String> packageForAverage = Arrays.asList("Basic", "Classic");
 
-		return recommendations;
+        for (InsurancePackageEntity insurancePackage : entities) {
+            if (vehicleEntity.getMileage() <= 10000) {
+                if (packageForNewCars.contains(insurancePackage.getPackageName()) && (userCurrentAge >= 18 && userCurrentAge <= 23)) {
+                    addPackageToList(insurancePackage, recommendations);
+                }
+            }
+            if (vehicleEntity.getMileage() > 10000 && vehicleEntity.getMileage() <= 100000 && (userCurrentAge >= 23 && userCurrentAge <= 45)) {
+                if (packageForAverage.contains(insurancePackage.getPackageName())) {
+                    addPackageToList(insurancePackage, recommendations);
+                }
+            }
+            if (vehicleEntity.getMileage() > 100000) {
+                if (packageForOldCars.contains(insurancePackage.getPackageName()) && (userCurrentAge > 45)) {
+                    addPackageToList(insurancePackage, recommendations);
+                }
+            }
+        }
 
-	}
+        return recommendations;
 
-	/***
-	 * This method is used to add the insurance package to the list of recommendations.
-	 * @param insurancePackage The insurance package to be
-	 * @param recommendations The list of recommendations to be
-	 */
-	private static void addPackageToList(InsurancePackageEntity insurancePackage,
-			ArrayList<InsurancePackageSchema> recommendations) {
-		Gson gson = new Gson();
+    }
 
-		InsurancePackageSchema packageSchema = gson.fromJson(gson.toJson(insurancePackage),
-				InsurancePackageSchema.class);
-		packageSchema.setPackageId(insurancePackage.getId());
-		recommendations.add(packageSchema);
-	}
+    /***
+     * This method is used to add the insurance package to the list of recommendations.
+     * @param insurancePackage The insurance package to be
+     * @param recommendations The list of recommendations to be
+     */
+    private static void addPackageToList(InsurancePackageEntity insurancePackage, ArrayList<InsurancePackageSchema> recommendations) {
+        Gson gson = new Gson();
+
+        InsurancePackageSchema packageSchema = gson.fromJson(gson.toJson(insurancePackage), InsurancePackageSchema.class);
+        packageSchema.setPackageId(insurancePackage.getId());
+        recommendations.add(packageSchema);
+    }
+
+
+    /***
+     * This method is used to calculate the age of the user.
+     * @param birthdate The date of birth of the user.
+     * @return The age of the user.
+     */
+    private static int calculateAge(String birthdate) {
+        LocalDate birthDate = LocalDate.parse(birthdate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate currentDate = LocalDate.now();
+        return Period.between(birthDate, currentDate).getYears();
+    }
 
 }
